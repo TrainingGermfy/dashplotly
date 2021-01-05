32adf26d09e27aef2ed258c1e68dd79b3734ac55
German Dominguez <gdominguezmontes@gmail.com>
2021-01-05 15:10:43 -0600

    First Commit

---
+++ 4.py
@@ -0,0 +1,70 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+
+app = dash.Dash()
+
+app.layout = html.Div(children=[
+    dcc.Dropdown(
+        options=[
+            {'label': 'New York City', 'value': 'NYC'},
+            {'label': u'Montréal', 'value': 'MTL'},
+            {'label': 'San Francisco', 'value': 'SF'}
+        ],
+        value='MTL'
+    ),
+    html.Label('Multi-Select Dropdown'),
+    dcc.Dropdown(
+        options=[
+            {'label': 'New York City', 'value': 'NYC'},
+            {'label': u'Montréal', 'value': 'MTL'},
+            {'label': 'San Francisco', 'value': 'SF'}
+        ],
+        value=['MTL', 'SF'],
+        multi=True
+    ),
+    html.Label('Radio Items'),
+    dcc.RadioItems(
+        options=[
+            {'label': 'New York City', 'value': 'NYC'},
+            {'label': u'Montréal', 'value': 'MTL'},
+            {'label': 'San Francisco', 'value': 'SF'}
+        ],
+        value='MTL'
+    ),
+    html.Label('Checkboxes'),
+    dcc.Checklist(
+        options=[
+            {'label': 'New York City', 'value': 'NYC'},
+            {'label': u'Montréal', 'value': 'MTL'},
+            {'label': 'San Francisco', 'value': 'SF'}
+        ],
+        value=['MTL', 'SF']
+    ),
+
+    html.Label('Text Box'),
+        dcc.Input(value='MTL', type='text'),
+    html.Div(id='mi-div2', children=[
+        html.Label('Input a text'),
+        dcc.Input(id='my-id', value='Dash App', type='text'),
+        html.Div(id='my-div'),
+    ])
+
+
+])
+
+
+
+
+
+
+@app.callback(
+    Output(component_id='my-div', component_property='children'),
+    [Input(component_id='my-id', component_property='value')])
+def actualiza_div(input_value):
+    return 'You\'ve entered "{}"'.format(input_value)
+
+
+if __name__ == '__main__':
+    app.run_server()
